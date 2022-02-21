3d6260791e34ef2ae93dfef5d1f2c79edc40070a
radisic.rade <radisic.rade@gmail.com>
2022-02-22 00:44:57 +0100

    Added LaTeX configuration

--- conf.py
+++ conf.py
@@ -65,5 +65,21 @@ highlight_language = 'c'
 # Code highlight theme
 pygments_style = 'tango'
 
-latex_engine = 'pdflatex'
+latex_engine = 'xelatex'
 
+latex_elements = {
+    'fontpkg': r'''
+\setmainfont{DejaVu Serif}
+\setsansfont{DejaVu Sans}
+\setmonofont{DejaVu Sans Mono}
+''',
+    'preamble': r'''
+\usepackage[titles]{tocloft}
+\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
+\setlength{\cftchapnumwidth}{0.75cm}
+\setlength{\cftsecindent}{\cftchapnumwidth}
+\setlength{\cftsecnumwidth}{1.25cm}
+''',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    'printindex': r'\footnotesize\raggedright\printindex',
+}
