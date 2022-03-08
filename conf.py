d76546860fcd5ff09bb71af05a7db42e0a516961
radisic.rade <radisic.rade@gmail.com>
2022-03-08 21:58:17 +0100

    Testing comments support by Disqus

--- conf.py
+++ conf.py
@@ -28,7 +28,8 @@ author = 'Srđan Popov, Rade Radišić'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ["sphinx_disqus.disqus"]
+disqus_shortname = "pjisp-zbirka-zadataka"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
