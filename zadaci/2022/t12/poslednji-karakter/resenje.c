3841dc536348504d708030c97b72fbfd34089681
radisic.rade <radisic.rade@gmail.com>
2023-11-06 01:04:00 +0100

    Fixed 2022 "Poslednji karakter" reference solution

--- zadaci/2022/t12/poslednji-karakter/resenje.c
+++ zadaci/2022/t12/poslednji-karakter/resenje.c
@@ -3,7 +3,8 @@
 
 #define DUZINA 100 + 1
 
-int main() {
+int main()
+{
     char polazni[DUZINA], krajnji[DUZINA], k;
     int brojac = 0, broj_reci = 0, i, ki = 0;
 
@@ -11,13 +12,19 @@ int main() {
     fgets(polazni, DUZINA, stdin);
 
     int duzina_polazni = strlen(polazni);
-    if(duzina_polazni < DUZINA - 1) polazni[strlen(polazni) - 1] = '\0';
+    if (polazni[duzina_polazni - 1] < '\n')
+    {
+        polazni[duzina_polazni - 1] = '\0';
+        duzina_polazni--;
+    }
 
     printf("Unesite karakter: ");
     scanf("%c", &k);
 
-    for (i = 0; i < strlen(polazni); i++) {
-        if ((polazni[i] >= 'A' && polazni[i] <= 'Z') || polazni[i] == ' ' || (polazni[i] >= 'a' && polazni[i] <= 'z')) {
+    for (i = 0; i < duzina_polazni; i++)
+    {
+        if ((polazni[i] >= 'A' && polazni[i] <= 'Z') || polazni[i] == ' ' || (polazni[i] >= 'a' && polazni[i] <= 'z'))
+        {
             krajnji[ki] = polazni[i];
             ki++;
         }
@@ -25,28 +32,39 @@ int main() {
 
     krajnji[ki] = '\0';
 
-    int poslednja_rec;
-    for (i = 0; i < strlen(krajnji); i++) {
-        if (krajnji[i] == ' ') {
+    int poslednja_rec, prethodno_slovo = 0;
+    for (i = 0; i < strlen(krajnji); i++)
+    {
+        if (krajnji[i] == ' ' && prethodno_slovo != 0)
+        {
             broj_reci++;
             poslednja_rec = i;
             if (krajnji[i - 1] == k)
             {
                 brojac++;
             }
+            prethodno_slovo = 0;
+        }
+        else if ((krajnji[i] >= 'A' && krajnji[i] <= 'Z') || (krajnji[i] >= 'a' && krajnji[i] <= 'z'))
+        {
+            prethodno_slovo = 1;
         }
     }
 
-    if (krajnji[strlen(krajnji) - 1] == k) {
+    if (krajnji[strlen(krajnji) - 1] == k)
+    {
         broj_reci++;
         brojac++;
-    } else {
+    }
+    else if (krajnji[strlen(krajnji) - 1] != ' ')
+    {
         broj_reci++;
     }
 
     printf("\nUdeo reci u polaznom stringu koji se zavrsavaju karakterom '%c' je %.2f%%.", k, ((float)brojac / broj_reci) * 100);
 
-    for (i = 0; i < strlen(krajnji); i++) {
+    for (i = 0; i < strlen(krajnji); i++)
+    {
         if (i % 2 == 0 && krajnji[i] >= 'a' && krajnji[i] <= 'z')
             krajnji[i] = krajnji[i] - 32;
     }
