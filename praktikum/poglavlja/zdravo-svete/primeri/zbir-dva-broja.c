bcea8bd43e890d75c03dd0dbb3e0be3ea2519a3c
radisic.rade <radisic.rade@gmail.com>
2024-02-26 00:35:24 +0100

    Add additional subchapter and remarks

---
+++ praktikum/poglavlja/zdravo-svete/primeri/zbir-dva-broja.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int prvi_broj, drugi_broj, zbir;
+
+    printf("Unesite prvi broj: ");
+    scanf("%d", &prvi_broj);
+
+    printf("Unesite prvi broj: ");
+    scanf("%d", &drugi_broj);
+
+    zbir = prvi_broj + drugi_broj;
+
+    printf("Zbir unetih brojeva je: %d\n", zbir);
+
+    return 0;
+}
