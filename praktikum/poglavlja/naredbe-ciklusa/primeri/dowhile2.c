878f0a7262f7beafb087e0c51e83c554eb058771
reljam01 <109527724+reljam01@users.noreply.github.com>
2024-03-01 19:05:56 +0100

    Praktikum petlje (#14)
    * Added loops to practicum
    
    * Added numbering for new chapter
    
    * Resolved changes
    
    * Removed wrong folder name
    
    * Fixed style

---
+++ praktikum/poglavlja/naredbe-ciklusa/primeri/dowhile2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int b, a = 2024;
+
+    printf("Pogodi broj!\n");
+    do 
+    {
+        scanf("%d", &b);
+        if (a > b) 
+        {
+            printf("Pokusaj ponovo, trazeni broj je veci od unetog!\n");
+        } 
+        else if (a < b)
+        {
+            printf("Pokusaj ponovo, trazeni broj je manji od unetog!\n");
+        }
+    } while(a != b);
+
+    printf("Cestitam! Trazeni broj je %d.\n", a);
+    return 0;
+}
